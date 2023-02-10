1728685a04a0c0d18215ff6b5455c04568e6d129
NikitaZhar <nikita.zharinov@gmail.com>
2023-02-10 14:14:33 +0100

    Commit 002. New menu. New random words selection. Array shuffle method. Wrong input number protection.

--- src/germanconsole/GermanConsole.java
+++ src/germanconsole/GermanConsole.java
@@ -5,98 +5,52 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class GermanConsole {
-
-	static WordOld[] listOfVerbs;
+	
+	private static Scanner console = new Scanner(System.in);
 	
 	public static void main(String[] args) {
 		
-		Verb verbs = new Verb();
+		Verbs verbs = new Verbs();
 		Noun nouns = new Noun();
-		
-		Scanner console = new Scanner(System.in);
-		
-		boolean continueLesson = true;
-		int selector = 0;
-		
-		while (continueLesson) {
-			System.out.println("\n\n\n\n\n\n\n\n\n\n       Menu       \n------------------");
-			System.out.println("0. Exit");
-			System.out.println("1. Verbs");
-			System.out.println("2. Nouns");
-			String selectorInput = console.nextLine();
-						
-			try {
-				selector = Integer.parseInt(selectorInput);
-			} catch (NumberFormatException nfe) {
-				System.out.println("Press number ...\n");
-				continue;
-			}
-			
-			switch(selector) {
-				case 0 -> continueLesson = false;
-				case 1 -> verbs.runTest();
-				case 2 -> nouns.runTest();
 				
-//				case 1 -> irregularVerbsTest();
-//				case 2 -> nounsTest();
-				default -> System.out.println("Choose from 0 to 2...\n\n\n\n\n\n\n\n\n");
+		boolean continueLesson = true;
+
+		while (continueLesson) {
+			switch(mainMenuInput()) {
+			case 0 -> continueLesson = false;
+			case 1 -> verbs.runTestOfVerbs();
+			case 2 -> nouns.runTest();
+			default -> System.out.println("Choose from 0 to 2...\n\n\n\n\n\n\n\n\n");
 			}			
 		}
-		
 	}
 	
-	static void irregularVerbsTest() {
-		Scanner console = new Scanner(System.in);
-		
-		initVerbs();
+	private static int mainMenuInput() {
 
-		System.out.println("Press 0 to exit");
+		int selector = 0;
 
 		while (true) {
-			Random rn = new Random();
-			int index = rn.nextInt(listOfVerbs.length);
-		
-			System.out.printf("%n%15s : %10s - %s - %s %n", listOfVerbs[index].getRussianWord(), 
-					listOfVerbs[index].getGermanWord(),
-					listOfVerbs[index].getPreteritum(),
-					listOfVerbs[index].getPartizip2());
+			String textBlock = """
+				   
+				   
+				   Available actions:
+
+					0. Exit
+					1. Verbs learning
+					2. Nouns learning
+
+					Enter a number : """;
+			System.out.printf(textBlock + "  ");
 			
-			int nextVerb = console.nextInt();			
-			if (nextVerb == 0) break;
+			String selectorInput = console.nextLine();
+			try {
+				selector = Integer.parseInt(selectorInput);
+				break;
+			} catch (NumberFormatException nfe) {
+				System.out.println("\n* * * Wrong selection! * * *\n\n\n");
+				continue;
+			}
 		}
-	}
-	
-	static void nounsTest() {
-		
-	}
-	
-	static void initVerbs() {
-		
-		ArrayList <WordOld> wordsList = new ArrayList();
-		
-		wordsList.add(new WordOld("есть", "essen", "aß", "gegessen"));
-		wordsList.add(new WordOld("пить", "trinken", "trank", "getrunken"));
-		wordsList.add(new WordOld("идти", "gehen", "ging", "gegangen"));
-		wordsList.add(new WordOld("бежать", "laufen", "lief", "gelaufen"));
-		wordsList.add(new WordOld("ехать", "fahren", "fuhr", "gefahren"));
-		wordsList.add(new WordOld("давать", "geben", "gab", "gegeben"));
-		wordsList.add(new WordOld("приходить", "kommen", "kam", "gekommen"));
-		wordsList.add(new WordOld("просить", "bitten", "bat", "gebeten"));
-		wordsList.add(new WordOld("оставаться", "bleiben", "blieb", "geblieben"));
-		wordsList.add(new WordOld("падать", "fallen", "fiel", "gefallen"));
-		wordsList.add(new WordOld("помогать", "helfen", "half", "geholfen"));
-		wordsList.add(new WordOld("читать", "lesen", "las", "gelesen"));
-		wordsList.add(new WordOld("лететь", "fliegen", "flog", "geflogen"));
-		wordsList.add(new WordOld("брать", "nehmen", "nahm", "genommen"));
-		wordsList.add(new WordOld("писать", "schreiben", "schrieb", "geschrieben"));
-		wordsList.add(new WordOld("находить", "finden", "fand", "gefunden"));
-		wordsList.add(new WordOld("петь", "singen", "sang", "gesungen"));
-		wordsList.add(new WordOld("закрывать", "schließen", "schloss", "geschlossen"));
-		wordsList.add(new WordOld("висеть", "hängen", "hing", "gehangen"));
-		wordsList.add(new WordOld("ловить", "fangen", "fing", "gefangen"));
-		
-		listOfVerbs = wordsList.toArray(new WordOld[0]);
-		
-//		listOfVerbs[0] = new Word("есть", "essen", "aß", "gegessen");
+		return selector;
 	}
 }
